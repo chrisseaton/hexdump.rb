93ad06e69719a50834210206dde84d8fb366d4a2
Chris Seaton <chris.seaton@shopify.com>
2021-08-09 01:02:16 +0100

    Use benchmark-ips

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ group :development do
   gem 'rspec',          '~> 3.0'
   gem 'kramdown'
   gem 'yard',           '~> 0.9'
+  gem 'benchmark-ips',  '~> 2.9.1'
 end
--- benchmark.rb
+++ benchmark.rb
@@ -3,44 +3,46 @@
 $LOAD_PATH.unshift(File.expand_path('../lib',__FILE__))
 
 require 'hexdump'
-require 'benchmark'
+require 'benchmark/ips'
 
-DATA = ((0..255).map { |b| b.chr }.join) * (1024 * 100)
+DATA = ((0..255).map { |b| b.chr }.join) * (1024 * 5)
 OUTPUT = Class.new { def <<(data); end }.new
 
-Benchmark.bm(33) do |b|
-  b.report('Hexdump.dump (output)') do
+puts RUBY_DESCRIPTION
+
+Benchmark.ips do |b|
+  b.report('H.d (output)') do
     Hexdump.dump(DATA, :output => OUTPUT)
   end
 
-  b.report('Hexdump.dump width=256 (output)') do
+  b.report('H.d w=256 (output)') do
     Hexdump.dump(DATA, width: 256, output: OUTPUT)
   end
 
-  b.report('Hexdump.dump ascii=true (output)') do
+  b.report('H.d a=true (output)') do
     Hexdump.dump(DATA, ascii: true, output: OUTPUT)
   end
 
   [2, 4, 8].each do |word_size|
-    b.report("Hexdump.dump word_size=#{word_size} (output)") do
+    b.report("H.d s=#{word_size} (output)") do
       Hexdump.dump(DATA, word_size: word_size, output: OUTPUT)
     end
   end
 
-  b.report('Hexdump.dump (block)') do
+  b.report('H.d (block)') do
     Hexdump.dump(DATA) { |index,hex,print| }
   end
 
-  b.report('Hexdump.dump width=256 (block)') do
+  b.report('H.d w=256 (block)') do
     Hexdump.dump(DATA, width: 256) { |index,hex,print| }
   end
 
-  b.report('Hexdump.dump ascii=true (block)') do
+  b.report('H.d a=true (block)') do
     Hexdump.dump(DATA, ascii: true) { |index,hex,print| }
   end
 
   [2, 4, 8].each do |word_size|
-    b.report("Hexdump.dump word_size=#{word_size} (block)") do
+    b.report("H.d s=#{word_size} (block)") do
       Hexdump.dump(DATA, word_size: word_size) { |index,hex,print| }
     end
   end
